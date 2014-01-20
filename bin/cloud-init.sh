173d99b1f6dfa577a060ddc4a65ac3358b254e9a
Gabriel Falcao <gabriel@nacaolivre.org>
2014-01-19 21:36:42 -0500

    fixing cloud init

--- bin/cloud-init.sh
+++ bin/cloud-init.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+hostname {subdomain}
+
 # Install saltstack
 add-apt-repository ppa:saltstack/salt -y
 apt-get update -y
@@ -8,8 +10,9 @@ apt-get install salt-minion -y
 
 export PAYLOAD=`echo "{{'ec2_hostname':'$(ec2metadata --public-hostname)','public_ip':'$(ec2metadata --public-ipv4)'}}" | sed s,"'",'\\\"',g`
 
-hostname {subdomain}
+
 sed -i "s/#master: salt/master: kush.weedlabs.io/" /etc/salt/minion
+sed -i "s/#id:/id: {subdomain} #/" /etc/salt/minion
 
 mkdir ~/.ssh
 
@@ -19,7 +22,6 @@ curl https://github.com/clarete.keys >> ~/.ssh/authorized_keys
 curl https://github.com/gabrielfalcao.keys >> ~/.ssh/authorized_keys
 
 chmod 600 ~/.ssh/authorized_keys
-
 
 
 cat <<- EOF > salt-minion.conf
@@ -44,6 +46,11 @@ EOF
 
 sudo cp salt-minion.conf /etc/init
 
-service salt-minion start
+service salt-minion start || echo 'started'
+service salt-minion stop || echo 'stopped'
 
 curl -X POST -H "Content-Type: application/json" -d "$PAYLOAD" http://kush.weedlabs.io/sms/{subdomain}
+
+curl http://kush.weedlabs.io
+
+service salt-minion restart || echo 'salt-minion-running'

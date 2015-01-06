d9cd2ad80e7613f094b49b4af20292becb5ba335
Gabriel Falcão <gabriel@nacaolivre.org>
2015-01-05 21:17:46 -0500

    updating ui component versions

--- kush/base/assets.py
+++ kush/base/assets.py
@@ -9,26 +9,26 @@ from flask.ext.assets import (
     Bundle,
 )
 
-jquery = Bundle('vendor/jquery/jquery.js')
+jquery = Bundle('vendor/jquery/dist/jquery.js')
 angular = Bundle('vendor/angular/angular.js')
 
 bootstrap_js = Bundle(
-    "vendor/uikit/src/js/core.js",
-    "vendor/uikit/src/js/touch.js",
-    "vendor/uikit/src/js/alert.js",
-    "vendor/uikit/src/js/button.js",
-    "vendor/uikit/src/js/dropdown.js",
-    "vendor/uikit/src/js/grid.js",
-    "vendor/uikit/src/js/modal.js",
-    "vendor/uikit/src/js/offcanvas.js",
-    "vendor/uikit/src/js/nav.js",
-    "vendor/uikit/src/js/tooltip.js",
-    "vendor/uikit/src/js/switcher.js",
-    "vendor/uikit/src/js/tab.js",
-    "vendor/uikit/src/js/search.js",
-    "vendor/uikit/src/js/scrollspy.js",
-    "vendor/uikit/src/js/smooth-scroll.js",
-    "vendor/uikit/src/js/toggle.js",
+    "vendor/uikit/js/core/core.js",
+    "vendor/uikit/js/core/touch.js",
+    "vendor/uikit/js/core/alert.js",
+    "vendor/uikit/js/core/button.js",
+    "vendor/uikit/js/core/dropdown.js",
+    "vendor/uikit/js/core/grid.js",
+    "vendor/uikit/js/core/modal.js",
+    "vendor/uikit/js/core/offcanvas.js",
+    "vendor/uikit/js/core/nav.js",
+    "vendor/uikit/js/core/tooltip.js",
+    "vendor/uikit/js/core/switcher.js",
+    "vendor/uikit/js/core/tab.js",
+    "vendor/uikit/js/components/search.js",
+    "vendor/uikit/js/core/scrollspy.js",
+    "vendor/uikit/js/core/smooth-scroll.js",
+    "vendor/uikit/js/core/toggle.js",
 )
 
 bootstrap_css = Bundle(
